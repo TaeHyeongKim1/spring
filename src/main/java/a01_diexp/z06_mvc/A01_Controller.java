73858c5e45b26f191b94cb4c44f69a65005c4d72
taehe <taehe@LAPTOP-DK23R8VQ>
2024-06-18 13:47:32 +0900

    111

--- src/main/java/a01_diexp/z06_mvc/A01_Controller.java
+++ src/main/java/a01_diexp/z06_mvc/A01_Controller.java
@@ -1,5 +1,23 @@
 package a01_diexp.z06_mvc;
 
-public class A01_Controller {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+public class A01_Controller {
+	@Autowired
+	private A02_Service service;
+	
+	
+	//요청처리 : url + 요청key/val
+	@RequestMapping("/empList.do")
+	public String getEmpList(String sch, Model d) {
+		d.addAttribute("empList", service.getList(sch));
+		System.out.println("서비스단 호출"+service.getList(sch));
+		return "";  //view단 호출
+	}
+	
+	
 }
--- src/main/java/a01_diexp/z06_mvc/A02_Service.java
+++ src/main/java/a01_diexp/z06_mvc/A02_Service.java
@@ -1,5 +1,16 @@
 package a01_diexp.z06_mvc;
 
-public class A02_Service {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
+public class A02_Service {
+	@Autowired(required =false)  //해당 객체가 container에 없더라도 nullpointer에러 발생 방지
+	private A03_Dao dao;  //인터페이스임에도 불구하고 아래와 같이 메서드에서 객체생성 메서드 호출 가능한 이유
+	//인터페이스(추상) = new 실제객체();
+	//참조.메서드() ==> 다형성에 의해서 실제 객체가 가지고 있는 메서드 활용
+	
+	public String getList(String sch) {
+		return dao.getList(sch);
+	}
 }
